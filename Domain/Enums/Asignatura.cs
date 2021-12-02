16582e281ae692bafe17f52b27d61f7dfea7650a
yaderxd <yaderoviedo4@gmail.com>
2021-12-02 09:18:55 -0600

    clase enums

---
+++ Domain/Enums/Asignatura.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum Asignatura
+    {
+        Contabilidad,
+        Sociologia,
+        Matematica_2,
+        INgles_2,
+        Algebra_Lineal,
+        Programacion_1,
+    }
+}
