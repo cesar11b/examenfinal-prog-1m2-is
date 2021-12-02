127be9e3029107a331f4e993b534c9919e08af8c
cesar jafet vivas solis <88453459+cesar11b@users.noreply.github.com>
2021-12-02 09:24:45 -0600

    Update Departamento_del_Estudiante.cs

--- Domain/Entities/Departamento_del_Estudiante.cs
+++ Domain/Entities/Departamento_del_Estudiante.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    class Departamento_del_Estudiante
+    public class Departamento_del_Estudiante
     {
+         public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellidos { get; set; }
+
+        public string Carnet { get; set; }
+
+        public Municipio Municipio { get; set; }
+
+        public Departamento Departamento { get; set; }
+
+        public int AlgebraLineal { get; set; }
+
+        public int Matematicas { get; set; }
+
+        public int ContabilidadDeCosto { get; set; }
+
+        public int InglesII { get; set; }
+
+        public int ProgramacionI { get; set; }
+
+        public int Sociologia { get; set; }
+        
     }
 }
