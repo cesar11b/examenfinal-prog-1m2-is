3608b3bb5b0eb899e1a09ef0d874846004ddf0ca
cesar jafet vivas solis <88453459+cesar11b@users.noreply.github.com>
2021-12-02 09:25:24 -0600

    Update Notas.cs

--- Domain/Entities/Notas.cs
+++ Domain/Entities/Notas.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    class Notas
+    public class Notas
     {
+         public Nota(Estudiante estudiante)
+        {
+            Estudiante = estudiante;
+        }
+
+        public Estudiante Estudiante { get; set; }
+
+        public Asignatura Asignatura { get; set; }
+
+        public int Sistematicos { get; set; }
+
+        public int ParcialI { get; set; }
+
+        public int ParcialII { get; set; }
+
+        public int NotaFinal { get; set; }
+
+        
+        
     }
 }
