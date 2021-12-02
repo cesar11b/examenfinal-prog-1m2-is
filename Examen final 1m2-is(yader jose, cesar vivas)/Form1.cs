82cb118f59a2753318b2706cca8b376f1f233fa9
cesar jafet vivas solis <88453459+cesar11b@users.noreply.github.com>
2021-12-02 08:27:39 -0600

    Add files via upload

---
+++ Examen final 1m2-is(yader jose, cesar vivas)/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examen_final_1m2_is_yader_jose__cesar_vivas_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Examen final 1m2-is(yader jose, cesar vivas)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examen_final_1m2_is_yader_jose__cesar_vivas_
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
